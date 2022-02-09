d222fecc48709562105e0e94e4c7977dc7261a7c
aaron <aaronmlucia@gmail.com>
2022-02-08 23:40:09 -0500

    stopping for the night 8 feb - unresolved issue with progress variable

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from surveys import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'la_cle_secrete'
+
+debug = DebugToolbarExtension(app)
 
 responses = []
 
+# To-do: reset the following on completion of the survey
 progress = 0
 
-# @app.route('/')
-# def show_survey_start():
-#     return render_template('base.html')
+@app.route('/')
+def show_survey_start():
+    return render_template('base.html')
 
-# @app.route(f'/questions/{progress}')
-# def show_questions():
-#     progress += 1
-#     return redirect(f'/questions/{progress}')
+@app.route(f'/questions/{progress}')
+def show_questions():
+    # reply = request.form[progress]
+    # responses.append(reply)
+    # progress += 1
+    print(progress)
+    return redirect(f'/questions/1')
